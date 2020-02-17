e92dd9eea7c307f61e6809815ba280c3b093de05
Alex Grigorev <alex.grigorev@hotmail.com>
2020-02-17 15:34:36 -0500

    Practise with Components Styling, and Inputs

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -2,15 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
+
 class App extends Component {
+  // state = {
+  //   persons: [
+  //     { name: 'Max', age: 28 },
+  //     { name: 'Manu', age: 29 },
+  //     { name: 'Stephanie', age: 26 }
+  //   ],
+  //   otherState: 'some other value'
+  // };
+
   state = {
-    persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
-    ],
-    otherState: 'some other value'
-  };
+    username: 'superalex' 
+  }
+
+  usernameChangedHandler = (event) => {
+    this.setState({username: event.target.value})
+  }
 
   switchNameHandler = (newName) => {
     // console.log('Was clicked!');
@@ -35,11 +47,33 @@ class App extends Component {
   }
 
   render() {
+
+    const style = {
+      backgroundColor: "white",
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
+
+
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
+
+      <UserInput 
+      changed={this.usernameChangedHandler}
+      currentName={this.state.username}
+      />
+      <UserOutput userName={this.state.username}/>
+      <UserOutput userName={this.state.username}/>
+      <UserOutput userName="Max"/>
+
+        {/* <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() =>  this.switchNameHandler('Joe!!!')}>Switch Name</button>
+        <button 
+          style={style}
+          onClick={() =>  this.switchNameHandler('Joe!!!')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -54,7 +88,7 @@ class App extends Component {
         <Person
           name={this.state.persons[2].name}
           age={this.state.persons[2].age}
-        />
+        /> */}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
